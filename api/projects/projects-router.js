683765b33ceea0a3bdb09e185ba0a86009842301
John Dee <johndeebdd@gmail.com>
2021-10-10 19:08:43 -0700

    working

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,16 @@
 // Write your "projects" router here!
+
+const { Server } = require("http");
+
+
+
+
+
+server.get('/api/projects',(req,res)=>{
+
+})
+
+
+server.get('/api/projects',(req,res)=>{
+
+})
