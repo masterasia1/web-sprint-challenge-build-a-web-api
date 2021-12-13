943d0e32b656a86b90d0850d8540da4739ff8b2e
masterasia1 <a.mayfield18@gmail.com>
2021-12-12 18:25:46 -0800

    working5

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,7 +1,43 @@
 // Write your "actions" router here!
 const express = require('express')
 
-const Actions = require('./actions-model')
+const Action = require('./actions-model')
 const router = express.Router ()
+
+router.get('/',(req,res)=>{
+    Action.get()
+    .then(actions => {
+        res.json(actions)
+    })
+    .catch(err => {
+        res.status()
+    }
+        )
+})
+
+router.get('/:id',(req,res)=>{
+    Action.get(req.params.id)
+    .then (stuff => {
+        if (stuff) {
+            res.status(200).json(stuff)
+        } else {
+            res.status(404).json()
+        }
+
+    })
+})
+
+router.post('/',(req,res)=>{
+
+})
+
+router.put('/:id',(req,res)=>{
+
+})
+
+router.delete('/:id',(req,res)=>{
+
+})
+
 
 module.exports = router
